3014e3323ea744905ce66a0da55875c78a8854da
Jordan <Luffy!99Piece>
2016-01-02 18:35:54 +0000

    Reformat  code

--- webapp/website/index.php
+++ webapp/website/index.php
@@ -13,34 +13,35 @@ require_once "tenancyCard.php";
     <link href="dashboard.css" rel="stylesheet">
 </head>
 <body>
- <nav class="navbar navbar-dark navbar-fixed-top bg-inverse">
-     <a class="navbar-brand" href="#">
-         <i class="fa fa-home"></i> Propman
-     </a>
-      <form class="pull-xs-left">
-          <input type="text" class="form-control search-bar" placeholder="Search...">
-      </form>
-      <div class="pull-xs-right navbar-icon-set">
-          <a class="btn btn-default navbar-icon search-icon" href="#"><i class="fa fa-search"></i></a>
-          <a class="btn btn-default navbar-icon" href="#"><i class="fa fa-plus"></i></a>
-          <a class="btn btn-default navbar-icon" href="#"><i class="fa fa-user"></i></a>
-          <a class="btn btn-default navbar-icon" href="#"><i class="fa fa-bell"></i></a>
-      </div>
- </nav>
+<nav class="navbar navbar-dark navbar-fixed-top bg-inverse">
+    <a class="navbar-brand" href="#">
+        <i class="fa fa-home"></i> Propman
+    </a>
+
+    <form class="pull-xs-left">
+        <input type="text" class="form-control search-bar" placeholder="Search...">
+    </form>
+    <div class="pull-xs-right navbar-icon-set">
+        <a class="btn btn-default navbar-icon search-icon" href="#"><i class="fa fa-search"></i></a>
+        <a class="btn btn-default navbar-icon" href="#"><i class="fa fa-plus"></i></a>
+        <a class="btn btn-default navbar-icon" href="#"><i class="fa fa-user"></i></a>
+        <a class="btn btn-default navbar-icon" href="#"><i class="fa fa-bell"></i></a>
+    </div>
+</nav>
 
 <div class="container-fluid main">
-     <h1>Dashboard cards</h1>
+    <h1>Dashboard cards</h1>
 
     <?php // demo cards
-    for($i=0; $i<16; $i++)
+    for ($i = 0; $i < 16; $i++)
         getPropertyCard(1);
     ?>
- </div>
+</div>
 
- <!-- Bootstrap core JavaScript
-    ================================================== -->
- <!-- Placed at the end of the document so the pages load faster -->
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
- <script src="bootstrap/js/bootstrap.min.js"></script>
+<!-- Bootstrap core JavaScript
+   ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
