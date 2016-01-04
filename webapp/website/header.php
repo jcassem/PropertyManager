e2de360a6aae4df0d000416f0971716fda1a86bd
Jordan <Luffy!99Piece>
2016-01-04 00:12:14 +0000

    Fix icons and styling: - button styling for plus modal - fix modal open shifting body to the left - fix icons in login modal

--- webapp/website/header.php
+++ webapp/website/header.php
@@ -87,8 +87,8 @@ _END;
         </div>
 
         <div class="modal-footer">
-          <button type="submit" class="btn btn-success btn-default pull-right" data-dismiss="modal"><i class="fa fa-floppy-o"></i> Save</button>
-          <button type="submit" class="btn btn-danger btn-default pull-right" data-dismiss="modal"><i class="fa fa-remove"></i> Cancel</button>
+          <button type="submit" class="btn btn-danger btn-default pull-right plus-modal-cancel" data-dismiss="modal"><i class="fa fa-remove"></i> Cancel</button>
+          <button type="submit" class="btn btn-success btn-default pull-right plus-modal-submit" data-dismiss="modal"><i class="fa fa-floppy-o"></i> Save</button>
         </div> <!-- footer -->
 
       </div> <!-- content -->
@@ -103,18 +103,18 @@ function get_login_modal()
 <div class="modal fade" id="loginModal" role="dialog">
     <div class="modal-dialog">
       <div class="modal-content">
-        <div class="modal-header" style="padding:35px 50px;">
+        <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4><span class="glyphicon glyphicon-lock"></span> Login</h4>
+          <h4><i class="fa fa-lock"></i> Login</h4>
         </div>
-        <div class="modal-body" style="padding:40px 50px;">
+        <div class="modal-body">
           <form role="form">
             <div class="form-group">
-              <label for="usrname"><span class="glyphicon glyphicon-user"></span> Username</label>
-              <input type="text" class="form-control" id="usrname" placeholder="Enter email">
+              <label for="username"><i class="fa fa-user"></i> Username</label>
+              <input type="text" class="form-control" id="username" placeholder="Enter email">
             </div>
             <div class="form-group">
-              <label for="psw"><span class="glyphicon glyphicon-eye-open"></span> Password</label>
+              <label for="psw"><i class="fa fa-key"></i> Password</label>
               <input type="text" class="form-control" id="psw" placeholder="Enter password">
             </div>
             <div class="checkbox">
@@ -124,7 +124,6 @@ function get_login_modal()
           </form>
         </div>
         <div class="modal-footer">
-          <!--button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal"><i class="fa fa-remove"></i> Cancel</button-->
           <p>Not a member? <a href="#">Sign Up</a></p>
           <p><a href="#">Forgot Password?</a></p>
         </div> <!-- footer -->
